dff2691e11c3981b1afecaf54344c9a058c7da79
Paloma Barros <86273706+PamBarros@users.noreply.github.com>
2022-01-08 16:46:22 -0300

    criação do estilo de todas as páginas

---
+++ JS/index.js
@@ -0,0 +1,144 @@
+//Fase 1 - Emily
+
+function  fa ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/gameover.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/desbloqueio-da-fase-dois-emily.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas!" ) ;
+        f () ;
+    }
+}
+
+//Fase 2 - Emily
+
+function  fb ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/desbloqueio-da-fase-tres-emily.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/gameover.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
+
+//Fase 3 - Emily
+
+function  fc ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/venceu.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/gameover.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
+
+
+//Fase 1 - Simon
+
+function  fd ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/desbloqueio-da-fase-dois-simon.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/gameover.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
+
+
+
+//Fase 2 - Simon
+
+function  fe ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/desbloqueio-da-fase-tres-simon.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/gameover.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
+
+
+//Fase 3 - Simon
+
+function  ff ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/gameover.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/venceu.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
+
+
+//Fase 1 - Melissa
+
+
+function  fg ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/gameover.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/desbloqueio-da-fase-dois-melissa.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
+
+
+//Fase 2 - Melissa
+
+function  fh ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/desbloqueio-da-fase-tres-melissa.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/gameover.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
+
+
+
+//Fase 3 - Melissa
+
+
+function  fi ( )  {
+    var  escolha  =  prompt ("O que Emily deve fazer ? \n A) \n B)").toLocaleUpperCase();
+
+    if ( escolha  ==  "A" ) {
+        location.assign("../HTML/gameover.html")
+    }  else  if  ( escolha  ==  "B" ) {
+        location.replace ("../HTML/venceu.html") ;
+    }  else  {
+        alert ( "Por favor, se atenha as opções propostas" ) ;
+        f () ;
+    }
+}
